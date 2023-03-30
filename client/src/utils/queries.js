7748278b0930852518f6ad7cdae33206fcd73bb3
Benmarz10 <benjaminmarz10@gmail.com>
2023-03-30 14:15:55 -0500

    fixed my query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,19 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
-  query me {
-    user {
-      _id
-      username
-      email
-      bookCount
-      savedBooks
+{
+  me {
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      image
+      link
+      title
+      description
     }
   }
-`;
+}`
